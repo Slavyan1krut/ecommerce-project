8516b59de5c66f89c91e4a9b81a0bc57bef5b2e5
Кайль В.С. 4315 <slavyan2krut@gmail.com>
2025-10-14 23:12:39 +0300

    добавлена страница корзины

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,8 @@ import './HomePage.css'
 export function HomePage() {
     return (
         <>
+        <title>Ecommerce project</title>
+
             <div className="header">
                 <div className="left-section">
                     <a href="index.html" className="header-link">
