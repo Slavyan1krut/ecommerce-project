5ac88e5e7401477b5fbea60779b73246ec470b38
Кайль В.С. 4315 <slavyan2krut@gmail.com>
2025-10-16 00:55:23 +0300

    Добавлена страница отслеживания заказа и подкорректированы ссылки и некоторые компоненты

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css'
 import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
-import { CheckoutPage } from './pages/CheckoutPage'
+import { CheckoutPage } from './pages/checkout/CheckoutPage'
 import { OrdersPage } from './pages/OrdersPage'
+import { TrackingPage } from './pages/TrackingPage'
 function App() {
 
     return (
@@ -11,6 +12,7 @@ function App() {
             <Route index element={<HomePage />} />
             <Route path="checkout" element={<CheckoutPage />} />
             <Route path="orders" element={<OrdersPage />} />
+            <Route path="tracking" element={<TrackingPage />} />
         </Routes>
         </>
     )
