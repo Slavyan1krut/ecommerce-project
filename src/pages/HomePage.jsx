21bae8ffc9b8ef24d2d9b65e29debb5c7a1a2ee2
Кайль В.С. 4315 <slavyan2krut@gmail.com>
2025-10-16 03:49:11 +0300

    Отображение количества предметов в корзине

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,12 +4,17 @@ import { useEffect, useState } from 'react';
 import { Header } from '../components/Header';
 
 export function HomePage() {
-    const[products, setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect(() => {
         axios.get('http://localhost:3000/api/products')
             .then((response) => {
                 setProducts(response.data)
+            })
+        axios.get('http://localhost:3000/api/cart-items')
+            .then((response) => {
+                setCart(response.data)
             })
     }, []);
 
@@ -18,7 +23,7 @@ export function HomePage() {
             <title>Ecommerce project</title>
             <link rel="icon" type="image" href="/home-favicon.png" />
 
-            <Header />
+            <Header cart={cart} />
 
             <div className="home-page">
                 <div className="products-grid">
