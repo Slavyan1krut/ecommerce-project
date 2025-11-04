295455d260e48e7f8ec150fc0c3cc5586e15b95b
Кайль В.С. 4315 <slavyan2krut@gmail.com>
2025-11-04 05:16:41 +0300

    Добавление товаров в корзину

--- src/pages/checkout/CheckoutHeader.jsx
+++ src/pages/checkout/CheckoutHeader.jsx
@@ -1,26 +1,31 @@
 import './CheckoutHeader.css'
 import { NavLink } from 'react-router';
 
-export function CheckoutHeader () {
+export function CheckoutHeader({ cart }) {
+    let totalQuantity = 0;
+
+    cart?.forEach(cartItem => {
+        totalQuantity += cartItem.quantity;
+    });
     return (
         <div className="checkout-header">
-                <div className="header-content">
-                    <div className="checkout-header-left-section">
-                        <NavLink to="/">
-                            <img className="logo" src="images/logo.png" />
-                            <img className="mobile-logo" src="images/mobile-logo.png" />
-                        </NavLink>
-                    </div>
+            <div className="header-content">
+                <div className="checkout-header-left-section">
+                    <NavLink to="/">
+                        <img className="logo" src="images/logo.png" />
+                        <img className="mobile-logo" src="images/mobile-logo.png" />
+                    </NavLink>
+                </div>
 
-                    <div className="checkout-header-middle-section">
-                        Checkout (<NavLink className="return-to-home-link"
-                            to="/">3 items</NavLink>)
-                    </div>
+                <div className="checkout-header-middle-section">
+                    Checkout (<NavLink className="return-to-home-link"
+                        to="/">{totalQuantity} items</NavLink>)
+                </div>
 
-                    <div className="checkout-header-right-section">
-                        <img src="images/icons/checkout-lock-icon.png" />
-                    </div>
+                <div className="checkout-header-right-section">
+                    <img src="images/icons/checkout-lock-icon.png" />
                 </div>
             </div>
+        </div>
     );
 }
